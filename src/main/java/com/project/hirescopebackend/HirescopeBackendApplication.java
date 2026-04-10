ba62207ac8e8ef93f5093d5b1882f3ce92cbc793
1anminJ <mjeoung413@naver.com>
2026-04-10 20:53:11 +0900

    feat: 초기 Dependencies 설정

---
+++ src/main/java/com/project/hirescopebackend/HirescopeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.hirescopebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirescopeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HirescopeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/hirescopebackend/HirescopeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hirescopebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirescopeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
